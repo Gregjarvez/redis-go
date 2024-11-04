3e557ecb3cf0ba01e17c578c14734bed19a29ef7
gregoryassasie <gregjarvez@gmail.com>
2024-11-04 19:14:22 +0000

    Handle multiple requests

--- app/server.go
+++ app/server.go
@@ -22,18 +22,22 @@ func main() {
 		os.Exit(1)
 	}
 
-	conn, err := l.Accept()
+	for {
+		conn, err := l.Accept()
 
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		go handleRequest(conn)
 	}
+}
 
-	defer conn.Close()
-
+func handleRequest(conn net.Conn) {
 	for {
 		args := make([]byte, 1024)
-		_, err = conn.Read(args)
+		_, err := conn.Read(args)
 
 		if err != nil {
 			if err == io.EOF {
@@ -42,15 +46,11 @@ func main() {
 			fmt.Println("Failed to read data: ", err.Error())
 			os.Exit(1)
 		}
+
 		_, err = conn.Write([]byte("+PONG\r\n"))
 
 		if err != nil {
-			fmt.Println("Failed to read data: ", err.Error())
-			os.Exit(1)
+			fmt.Println("Failed to write response: ", err.Error())
 		}
-	}
-
-	if err != nil {
-		fmt.Println("Failed to handle connection: ", err.Error())
 	}
 }

cd9ad851d2b652ccc15d5faece1a8a9668700a10
gregoryassasie <gregjarvez@gmail.com>
2025-02-08 03:25:27 +0000

    codecrafters submit [skip ci]

--- app/tcp/slave.go
+++ app/tcp/slave.go
@@ -108,6 +108,8 @@ func (ss *SlaveServer) connectToMaster() {
 		fmt.Println("Error hydrating datastore: ", err)
 	}
 
+	r, err := rw.Peek(50)
+	fmt.Println("Handshake response: ", string(r))
 	ss.handleConnection(conn)
 }
 
